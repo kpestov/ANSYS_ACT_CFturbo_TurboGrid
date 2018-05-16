a90874a0d1049cf5b3c398d656aac5c4738e3dab
Kirill <kpestov91@gmail.com>
2018-05-16 09:42:02 +0500

    Moved all functions from xml_parser.py to main_kp.py

--- CFTurboExtension_kp/main_kp.py
+++ CFTurboExtension_kp/main_kp.py
@@ -7,7 +7,8 @@ from filecmp import cmp
 from ntpath import basename
 from os import remove, path, environ, system
 from System.IO import Path
-from xml_parser import impeller_main_dimensions
+# from xml_parser import impeller_main_dimensions
+import xml.etree.ElementTree as ET
 
 
 def InFileValid(task, property):
@@ -21,12 +22,15 @@ def edit(task):
 
     # obtain data container reference
     container = task.InternalObject
+    active_dir = container.GetActiveDirectory()
 
     # prepare arguments and open file selection dialog starting in user_files
     Filters = "CFTurbo batch (*.cft-batch)|*.cft-batch|All files (*.*)|*.*"
     FilterIndex = 0
     startDir = GetUserFilesDirectory()
     diagResult = FileDialog.ShowOpenDialog(Window.MainWindow, startDir, Filters, FilterIndex, "", "")
+
+
 
     if diagResult[0] == DialogResult.OK:
         group = task.Properties["CFTurbo batch file"]
@@ -63,11 +67,28 @@ def edit(task):
                 fileRef = RegisterFile(FilePath=filePath.Value)
                 AssociateFileWithContainer(fileRef, container)
 
-    # function that takes values from a file .cft-batch
-    update_main_dimensions(task)
+    # get_xml_root
+    tree = ET.parse(active_dir + r"\test-impeller.cft-batch")
+    root = tree.getroot()
+
+    cfturbo_batch_project_node = root.find('CFturboBatchProject')
+    updates_node = cfturbo_batch_project_node.find('Updates')
+    cfturbo_project_node = updates_node.find('CFturboProject')
+
+    for impeller_design_node in cfturbo_project_node:
+        main_dimensions = {}
+        main_dimensions_node = impeller_design_node.find('MainDimensions')
+        main_dimensions_sub_node = main_dimensions_node.find('MainDimensionsElement')
+
+        for child in main_dimensions_sub_node:
+            main_dimensions[child.tag] = child.text
+
+        # function that takes values from a file .cft-batch
+        update_main_dimensions(task, main_dimensions)
 
 
-def update_main_dimensions(task):
+def update_main_dimensions(task, main_dimensions):
+
     group = task.Properties["MainDimensions"]
 
     tip_clearance = group.Properties["TipClearance"]
@@ -76,9 +97,9 @@ def update_main_dimensions(task):
     impeller_diameter = group.Properties["ImpellerDiameter"]
     impeller_outlet_width = group.Properties["ImpellerOutletWidth"]
 
-    tip_clearance.Value = impeller_main_dimensions['xTip']
-    hub_diameter.Value = impeller_main_dimensions['dN']
-    suction_diameter.Value = impeller_main_dimensions['dS']
-    impeller_diameter.Value = impeller_main_dimensions['d2']
-    impeller_outlet_width.Value = impeller_main_dimensions['b2']
+    tip_clearance.Value = main_dimensions['xTip']
+    hub_diameter.Value = main_dimensions['dN']
+    suction_diameter.Value = main_dimensions['dS']
+    impeller_diameter.Value = main_dimensions['d2']
+    impeller_outlet_width.Value = main_dimensions['b2']
 
--- CFTurboExtension_kp/xml_parser.py
+++ CFTurboExtension_kp/xml_parser.py
@@ -1,37 +1,59 @@
 import xml.etree.ElementTree as ET
 
+
 FILE = r'C:\ansys_projects\act\kp\CFturbo_files\test-impeller.cft-batch'
+# FILE = task.ActiveDirectory + r"\test-impeller.cft-batch"
 
 
-def get_xml_root():
-    tree = ET.parse(FILE)
-    root = tree.getroot()
-    return root
+# def get_xml_root():
+#     tree = ET.parse(FILE)
+#     root = tree.getroot()
+#     return root
+#
+#
+# def get_design_impeller_node(root):
+#     cfturbo_batch_project_node = root.find('CFturboBatchProject')
+#     updates_node = cfturbo_batch_project_node.find('Updates')
+#     cfturbo_project_node = updates_node.find('CFturboProject')
+#
+#     for impeller_design_node in cfturbo_project_node:
+#         return impeller_design_node
+#
+#
+# def get_impeller_main_dimensions(impeller_design_node):
+#     main_dimensions = {}
+#     main_dimensions_node = impeller_design_node.find('MainDimensions')
+#     main_dimensions_sub_node = main_dimensions_node.find('MainDimensionsElement')
+#
+#     # in future for dict keys use attribute names instead tags since they are unique and the tags can intersect
+#     for child in main_dimensions_sub_node:
+#         main_dimensions[child.tag] = child.text
+#     return main_dimensions
+#
+#
+# xml_root = get_xml_root()
+# design_impeller_node = get_design_impeller_node(xml_root)
+# impeller_main_dimensions = get_impeller_main_dimensions(design_impeller_node)
+# print(impeller_main_dimensions)
 
 
-def get_design_impeller_node(root):
-    cfturbo_batch_project_node = root.find('CFturboBatchProject')
-    updates_node = cfturbo_batch_project_node.find('Updates')
-    cfturbo_project_node = updates_node.find('CFturboProject')
+tree = ET.parse(FILE)
+root = tree.getroot()
 
-    for impeller_design_node in cfturbo_project_node:
-        return impeller_design_node
+cfturbo_batch_project_node = root.find('CFturboBatchProject')
+updates_node = cfturbo_batch_project_node.find('Updates')
+cfturbo_project_node = updates_node.find('CFturboProject')
 
-
-def get_impeller_main_dimensions(impeller_design_node):
+for impeller_design_node in cfturbo_project_node:
     main_dimensions = {}
     main_dimensions_node = impeller_design_node.find('MainDimensions')
     main_dimensions_sub_node = main_dimensions_node.find('MainDimensionsElement')
 
-    # in future for dict keys use attribute names instead tags since they are unique, and the tags can intersect
     for child in main_dimensions_sub_node:
         main_dimensions[child.tag] = child.text
-    return main_dimensions
 
+    print(main_dimensions)
 
-xml_root = get_xml_root()
-design_impeller_node = get_design_impeller_node(xml_root)
-impeller_main_dimensions = get_impeller_main_dimensions(design_impeller_node)
 
 
 
