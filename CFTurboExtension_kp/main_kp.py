2c10e703df2515d22c62e513fe269c076503baaf
Kirill <kpestov91@gmail.com>
2018-08-02 17:13:51 +0500

    fixed bugs that were associated with reading .cft-batch file

--- CFTurboExtension_kp/main_kp.py
+++ CFTurboExtension_kp/main_kp.py
@@ -1,19 +1,14 @@
 import clr
 import os
 import xml.etree.ElementTree as ET
-import subprocess
-import re
 clr.AddReference("Ans.UI.Toolkit")
 clr.AddReference("Ans.UI.Toolkit.Base")
 clr.AddReference('Ansys.ACT.Interfaces')
-# clr.AddReference('System.Windows.Forms')
-# from System.Windows.Forms import MessageBox, MessageBoxButtons, MessageBoxIcon
 from Ansys.ACT.Interfaces.Workflow import *
 from Ansys.UI.Toolkit import *
-from shutil import copyfile, copy
-from filecmp import cmp
+from shutil import copyfile
 from ntpath import basename
-from os import remove, path, environ, system
+from os import remove, path, environ
 from System.IO import Path
 from System.Diagnostics import Process
 
@@ -57,18 +52,36 @@ def copy_cft_file(task):
     input_file = cfturbo_batch_project_node.attrib['InputFile']
 
     # obtain .cft file name with extension
-    file_name = os.path.basename(input_file)
+    file_name = path.basename(input_file)
     source_dir = Path.Combine(start_dir, file_name)
     target_dir = Path.Combine(task.ActiveDirectory, file_name)
     try:
         copyfile(source_dir, target_dir)
     except:
         # MessageBox.Show('hui', 'hui_2', MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
-        MessageBox.Show(Window.MainWindow, 'Failed to copy cft file to the working directory! Please place the .cft file '
+        MessageBox.Show(Window.MainWindow, 'Failed to copy .cft file to the working directory! Please place the .cft file '
                                          'in the user_files directory', 'Warning', MessageBoxType.Error, MessageBoxButtons.OK)
 
     # changes .cft file dir in .cft-batch file when copy .cft-batch to ActiveDirectory
     cfturbo_batch_project_node.attrib['InputFile'] = target_dir
+
+    # find first and second BatchAction nodes
+    batch_action_node = root[0][1]
+    batch_action_2nd_node = root[0][2]
+
+    # write values for selected attribs
+    batch_action_node.attrib['ExportInterface'] = "TurboGrid"
+    batch_action_node.attrib['WorkingDir'] = ""
+    batch_action_2nd_node.attrib['OutputFile'] = ""
+
+    # check if ExportComponents element exist in .cft-batch file in parent element BatchAction
+    # (element 'BatchAction' = root[0][1])
+    if list(root[0][1]) == []:
+        pass
+    else:
+        export_components_node = root[0][1].find('ExportComponents')
+        root[0][1].remove(export_components_node)
+
     tree.write(target_dir + '-batch')
 
     file_ref = RegisterFile(FilePath=target_dir)
@@ -133,12 +146,13 @@ def edit(task):
     # get main dimension xml node from .cft-batch file
     main_dimensions_sub_node = get_main_dim_node(task)
     main_dimensions = {}
+
+    # in future for dict keys use attribute names instead tags since they are unique and the tags can intersect
     for child in main_dimensions_sub_node:
         main_dimensions[child.tag] = child.text
 
     # function which takes values from a file .cft-batch
     get_main_dimensions(task, main_dimensions)
-
 
 
 def get_cft_path(task):
@@ -215,6 +229,10 @@ def update(task):
     group = task.Properties["MainDimensions"]
     tip_clearance = group.Properties["TipClearance"]
     hub_diameter = group.Properties["HubDiameter"]
+    suction_diameter = group.Properties["SuctionDiameter"]
+    impeller_diameter = group.Properties["ImpellerDiameter"]
+    impeller_outlet_width = group.Properties["ImpellerOutletWidth"]
+
     tree = get_xml_tree(task)
 
     # get_xml_root
@@ -230,11 +248,22 @@ def update(task):
         main_dimensions_node = impeller_design_node.find('MainDimensions')
         main_dimensions_sub_node = main_dimensions_node.find('MainDimensionsElement')
 
-        child_0 = main_dimensions_sub_node.find('xTip')
-        child_1 = main_dimensions_sub_node.find('dN')
+        # the code bellow writes new values of parameter when update cell#2
+        tip_clearance_value = main_dimensions_sub_node.find('xTip')
+        hub_diameter_value = main_dimensions_sub_node.find('dN')
+        suction_diameter_value = main_dimensions_sub_node.find('dS')
+        impeller_diameter_value = main_dimensions_sub_node.find('d2')
+        impeller_outlet_width_value = main_dimensions_sub_node.find('b2')
 
-        child_0.text = str(tip_clearance.Value)
-        child_1.text = str(hub_diameter.Value)
+        tip_clearance_value.text = str(tip_clearance.Value)
+        hub_diameter_value.text = str(hub_diameter.Value)
+
+        # I've commented it because axial pump have no <d2> and <b2> tags in .cft-batch file. When the program is
+        # executed there is an error shown 'impeller_diameter_value var have no attrib text'
+
+        # suction_diameter_value.text = str(suction_diameter.Value)
+        # impeller_diameter_value.text = str(impeller_diameter.Value)
+        # impeller_outlet_width_value.text = str(impeller_outlet_width.Value)
 
         target_dir = copy_cft_file(task)
         tree.write(target_dir + '-batch')
@@ -245,7 +274,10 @@ def launch_cfturbo(task):
     cft_path = environ.get(cft_env)
     launch_cft_path = os.path.join(cft_path, 'cfturbo.exe')
     cft_batch_file = get_cft_path(task)
-    start_cfturbo = Process.Start(launch_cft_path, '-batch' + ' ' + cft_batch_file)
+
+    quoted_cft_batch_file ='"{}"'.format(cft_batch_file)
+
+    start_cfturbo = Process.Start(launch_cft_path, '-batch' + ' ' + quoted_cft_batch_file)
     start_cfturbo.WaitForExit()
     exit_code = start_cfturbo.ExitCode
 
@@ -255,7 +287,7 @@ def launch_cfturbo(task):
 
 def obtain_cft_file_name(task):
     cft_file_path = get_cft_path(task)
-    cft_file_name = basename(cft_file_path).split('.')[0]
+    cft_file_name = basename(cft_file_path).split('.cft')[0]
 
     return cft_file_name
 
@@ -267,114 +299,77 @@ def obtain_tse_file(task):
 
     return tse_file
 
-class Geometry:
 
-    def extract_attribs(self, tse_file, start, end):
-        attribs = {}
-        with open(tse_file) as f:
-            for line in f:
-                if line.strip() == start:
-                    break
+def extract_attribs(task, start, end):
 
-            for line in f:
-                if line.strip() == end:
-                    break
-
-                data = line.strip().split(' = ')
-                new_keys = re.sub(' ', '_', data[0])
-                attribs[new_keys] = data[1]
-
-        return attribs
-
-machine_data = Geometry()
-hub = Geometry()
-shroud = Geometry()
-main_blade = Geometry()
-splitter_blade = Geometry()
-
-
-def make_machine_data_attribs(task):
     tse_file = obtain_tse_file(task)
-    machine_data_attribs = machine_data.extract_attribs(tse_file, 'MACHINE DATA:', 'END')
+    attribs = {}
+
+    with open(tse_file) as f:
+        for line in f:
+            if line.strip() == start:
+                break
+
+        for line in f:
+            if line.strip() == end:
+                break
+
+            data = line.strip().split(' = ')
+            attribs[data[0]] = data[1]
+
+    return attribs
+
+
+def extract_machine_data_attribs(task):
+    machine_data_attribs = extract_attribs(task, 'MACHINE DATA:', 'END')
 
     return machine_data_attribs
 
 
-def make_hub_attribs(task):
-    tse_file = obtain_tse_file(task)
-    hub_attribs = hub.extract_attribs(tse_file, 'HUB:', 'END')
+def extract_hub_attribs(task):
+    hub_attribs = extract_attribs(task, 'HUB:', 'END')
 
     return hub_attribs
 
 
-def make_shroud_attribs(task):
-    tse_file = obtain_tse_file(task)
-    shroud_attribs = shroud.extract_attribs(tse_file, 'SHROUD:', 'END')
+def extract_shroud_attribs(task):
+    shroud_attribs = extract_attribs(task, 'SHROUD:', 'END')
 
     return shroud_attribs
 
 
-def make_main_blade_attribs(task):
-    tse_file = obtain_tse_file(task)
-    main_blade_attribs = main_blade.extract_attribs(tse_file, 'BLADE:Main blade', 'END')
+def extract_main_blade_attribs(task):
+    main_blade_attribs = extract_attribs(task, 'BLADE:Main blade', 'END')
 
     return main_blade_attribs
 
 
-def make_splitter_blade_attribs(task):
-    tse_file = obtain_tse_file(task)
-    splitter_blade_attribs = splitter_blade.extract_attribs(tse_file, 'BLADE:Splitter blade', 'END')
+def extract_splitter_blade_attribs(task):
+    splitter_blade_attribs = extract_attribs(task, 'BLADE:Splitter blade', 'END')
 
     return splitter_blade_attribs
 
 
-def make_MachineData_class(task):
-    machine_data_attribs = make_machine_data_attribs(task)
-    MachineData = type('MachineData', (), machine_data_attribs)
-
-    return MachineData
-
-
-def make_Hub_class(task):
-    hub_attribs = make_hub_attribs(task)
-    Hub = type('Hub', (), hub_attribs)
-
-    return Hub
-
-
-def make_Shroud_class(task):
-    shroud_attribs = make_shroud_attribs(task)
-    Shroud = type('Shroud', (), shroud_attribs)
-
-    return Shroud
-
-
-def make_MainBlade_class(task):
-    main_blade_attribs = make_main_blade_attribs(task)
-    MainBlade = type('MainBlade', (), main_blade_attribs)
-
-    return MainBlade
-
-
 def make_s4(task):
-    splitter_blade_attribs = make_splitter_blade_attribs(task)
-    MainBlade = make_MainBlade_class(task)
+
+    splitter_blade_attribs = extract_splitter_blade_attribs(task)
+    main_blade_attribs = extract_main_blade_attribs(task)
 
     if splitter_blade_attribs == {}:
-        blades_per_set = int(MainBlade.Blade_Number)
+        blades_per_set = int(main_blade_attribs['Blade Number'])
         s4 = 'Number of Blades Per Set: {}'.format(blades_per_set + 1)
     else:
-        SplitterBlade = type('SplitterBlade', (), splitter_blade_attribs)
-        blades_per_set = int(SplitterBlade.Blade_Number)
+        blades_per_set = int(splitter_blade_attribs['Blade Number'])
         s4 = 'Number of Blades Per Set: {}'.format(blades_per_set + 1)
 
     return s4
 
 
 def make_s8(task):
-    MainBlade = make_MainBlade_class(task)
 
-    blade_0_le_type = MainBlade.Leading_Edge_Type
+    main_blade_attribs = extract_main_blade_attribs(task)
+
+    blade_0_le_type = main_blade_attribs['Leading Edge Type']
     if blade_0_le_type == 'Single':
         blade_0_le_type = 'EllipseEnd'
     else:
@@ -385,26 +380,27 @@ def make_s8(task):
 
 
 def make_s9(task):
-    MainBlade = make_MainBlade_class(task)
 
-    blade_0_te_type = MainBlade.Trailing_Edge_Type
+    main_blade_attribs = extract_main_blade_attribs(task)
+
+    blade_0_te_type = main_blade_attribs['Trailing Edge Type']
     if blade_0_te_type == 'Single':
         blade_0_te_type = 'EllipseEnd'
     else:
         blade_0_te_type = 'CutOffEnd'
-    s9 = 'Blade 0 LE: {}'.format(blade_0_te_type)
+    s9 = 'Blade 0 TE: {}'.format(blade_0_te_type)
 
     return s9
 
 
 def make_s10(task):
-    splitter_blade_attribs = make_splitter_blade_attribs(task)
+
+    splitter_blade_attribs = extract_splitter_blade_attribs(task)
 
     if splitter_blade_attribs == {}:
         return
     else:
-        SplitterBlade = type('SplitterBlade', (), splitter_blade_attribs)
-        blade_1_le_type = SplitterBlade.Leading_Edge_Type
+        blade_1_le_type = splitter_blade_attribs['Leading Edge Type']
         if blade_1_le_type == 'Single':
             blade_1_le_type = 'EllipseEnd'
         else:
@@ -415,13 +411,13 @@ def make_s10(task):
 
 
 def make_s11(task):
-    splitter_blade_attribs = make_splitter_blade_attribs(task)
+
+    splitter_blade_attribs = extract_splitter_blade_attribs(task)
 
     if splitter_blade_attribs == {}:
         return
     else:
-        SplitterBlade = type('SplitterBlade', (), splitter_blade_attribs)
-        blade_1_te_type = SplitterBlade.Trailing_Edge_Type
+        blade_1_te_type = splitter_blade_attribs['Trailing Edge Type']
         if blade_1_te_type == 'Single':
             blade_1_te_type = 'EllipseEnd'
         else:
@@ -436,27 +432,26 @@ def create_inf_file(task):
     inf_file = cft_file_name + '.inf'
     active_dir = os.path.join(task.ActiveDirectory, inf_file)
 
-    MachineData = make_MachineData_class(task)
-    Hub = make_Hub_class(task)
-    Shroud = make_Shroud_class(task)
-    MainBlade = make_MainBlade_class(task)
+    machine_data_attribs = extract_machine_data_attribs(task)
+    hub_attribs = extract_hub_attribs(task)
+    shroud_attribs = extract_shroud_attribs(task)
+    main_blade_attribs = extract_main_blade_attribs(task)
 
     s1 = '!======  CFTurbo Export  ========'
-    s2 = 'Axis of Rotation: {}'.format(MachineData.Principal_Axis)
-    s3 = 'Number of Blade Sets: {}'.format(MachineData.Bladeset_Count)
+    s2 = 'Axis of Rotation: {}'.format(machine_data_attribs['Principal Axis'])
+    s3 = 'Number of Blade Sets: {}'.format(machine_data_attribs['Bladeset Count'])
+    s4 = make_s4(task)
     s5 = 'Blade Loft Direction: Spanwise'
-    geometry_units = Hub.Input_Length_Units
+    geometry_units = hub_attribs['Input Length Units']
     s6 = 'Geometry Units: {}'.format(geometry_units.upper())
     s7 = 'Coordinate System Orientation: Righthanded'
-    s12 = 'Hub Data File: {}'.format(path.basename(Hub.Input_Filename))
-    s13 = 'Shroud Data File: {}'.format(path.basename(Shroud.Input_Filename))
-    s14 = 'Profile Data File: {}'.format(path.basename(MainBlade.Input_Filename))
-
-    s4 = make_s4(task)
     s8 = make_s8(task)
     s9 = make_s9(task)
     s10 = make_s10(task)
     s11 = make_s11(task)
+    s12 = 'Hub Data File: {}'.format(path.basename(hub_attribs['Input Filename']))
+    s13 = 'Shroud Data File: {}'.format(path.basename(shroud_attribs['Input Filename']))
+    s14 = 'Profile Data File: {}'.format(path.basename(main_blade_attribs['Input Filename']))
 
     with open(active_dir, 'w') as f:
         f.write(s1 + '\n')
@@ -480,60 +475,47 @@ def create_inf_file(task):
         f.write(s14 + '\n')
 
 
-# def cfturbo_start(task):
-#     launch_cfturbo(task)
-#
-#     # here will be function which extract information from .tse file and create .inf file
-#     # create_inf_file()
+def cfturbo_start(task):
 
-def dummy_copying(task):
-
-# this is dummy function which copies .curve and .tse files from act directory to task.ActiveDirectory
+    launch_cfturbo(task)
 
     container = task.InternalObject
     fileRef = None
 
-    extensionDir = ExtAPI.ExtensionManager.CurrentExtension.InstallDir
+    cft_file_name = obtain_cft_file_name(task)
+    inf_file = cft_file_name + '.inf'
+    inf_file_dir = os.path.join(task.ActiveDirectory, inf_file)
 
-    # obtain source and target directories for copy .curve, .tse files (this is dummy code)
-    for TurboGridFiles in os.listdir(extensionDir):
-        if TurboGridFiles.endswith(".curve") or TurboGridFiles.endswith(".tse"):
-            source_files = os.path.join(extensionDir, TurboGridFiles)
-            target_files = os.path.join(task.ActiveDirectory, TurboGridFiles)
-
-            # copy .curve, .tse files from act directory to task.ActiveDirectory (this is dummy code)
-            copy(source_files, target_files)
-
+    # function which extracts information from .tse file and create .inf file
     create_inf_file(task)
 
-    # here will be function which extract information from .tse file and create .inf file
-    # create_inf_file()
-
-
-    # here wiil be name of .inf file for registration
-    # TurboGridInputFiles =
+    # .inf file registration
+    TurboGridInputFiles = inf_file_dir
 
     # check if the .inf file is associated with WB project
+    isRegistered = IsFileRegistered(FilePath=TurboGridInputFiles)
+    if isRegistered == True:
+        fileRef = GetRegisteredFile(FilePath=TurboGridInputFiles)
+    else:
+        fileRef = RegisterFile(FilePath=TurboGridInputFiles)
+        AssociateFileWithContainer(fileRef, container)
 
-    # isRegistered = IsFileRegistered(FilePath=TurboGridInputFiles)
-    # if isRegistered == True:
-    #     fileRef = GetRegisteredFile(FilePath=TurboGridInputFiles)
-    # else:
-    #     fileRef = RegisterFile(FilePath=TurboGridInputFiles)
-    #     AssociateFileWithContainer(fileRef, container)
-    #
-    # outputRefs = container.GetOutputData()
-    # outputSet = outputRefs["TurboGeometry"]
-    # myData = outputSet[0]
-    # myData.INFFilename = fileRef
+    outputRefs = container.GetOutputData()
+    outputSet = outputRefs["TurboGeometry"]
+    myData = outputSet[0]
+    myData.INFFilename = fileRef
 
 
 # def dummy_copying(task):
 #
-# # this is dummy function which copies .curve, .tse and .inf files from act directory to task.ActiveDirectory
+# # this is dummy function which copies .curve and .tse files from act directory to task.ActiveDirectory
 #
 #     container = task.InternalObject
 #     fileRef = None
+#
+#     cft_file_name = obtain_cft_file_name(task)
+#     inf_file = cft_file_name + '.inf'
+#     inf_file_dir = os.path.join(task.ActiveDirectory, inf_file)
 #
 #     extensionDir = ExtAPI.ExtensionManager.CurrentExtension.InstallDir
 #
@@ -546,12 +528,13 @@ def dummy_copying(task):
 #             # copy .curve, .tse files from act directory to task.ActiveDirectory (this is dummy code)
 #             copy(source_files, target_files)
 #
-#     # obtain source and target directories for copy .inf file (this is dummy code)
-#     TurboGridDummyFiles = System.IO.Path.Combine(extensionDir, "BG.inf")
-#     TurboGridInputFiles = System.IO.Path.Combine(task.ActiveDirectory, "BG.inf")
+#     # here will be function which extract information from .tse file and create .inf file
+#     create_inf_file(task)
+#
+#     # here wiil be name of .inf file for registration
+#     TurboGridInputFiles = inf_file_dir
 #
 #     # check if the .inf file is associated with WB project
-#
 #     isRegistered = IsFileRegistered(FilePath=TurboGridInputFiles)
 #     if isRegistered == True:
 #         fileRef = GetRegisteredFile(FilePath=TurboGridInputFiles)
@@ -559,13 +542,11 @@ def dummy_copying(task):
 #         fileRef = RegisterFile(FilePath=TurboGridInputFiles)
 #         AssociateFileWithContainer(fileRef, container)
 #
-#     # copy .inf file from act directory to task.ActiveDirectory (this is dummy code)
-#     copy(TurboGridDummyFiles, TurboGridInputFiles)
-#
 #     outputRefs = container.GetOutputData()
 #     outputSet = outputRefs["TurboGeometry"]
 #     myData = outputSet[0]
 #     myData.INFFilename = fileRef
+
 
 
 
