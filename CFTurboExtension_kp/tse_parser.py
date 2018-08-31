3d57743b135ccfbbc76b1426b0f34e61cfcc0ca4
Kirill <kpestov91@gmail.com>
2018-08-31 16:09:28 +0500

    rewrote tse_parser.py and replaced code which is responsible for visibility of properties to the new module visibility.py

--- CFTurboExtension_kp/tse_parser.py
+++ CFTurboExtension_kp/tse_parser.py
@@ -1,180 +1,155 @@
+import re
 from os import path
+from ntpath import basename
 
 
-def obtain_tse_file():
-    tse_file = 'Splitter Impeller optim_Co1.tse'
+class Geometry:
+    def __init__(self, task):
+        self.task = task
 
-    return tse_file
+    def obtain_cft_file_name(self, task):
+        cft_file_path = Impeller(task).get_cft_batch_path(task)
+        cft_file_name = basename(cft_file_path).split('.cft')[0]
 
-def extract_attribs(start, end):
+        return cft_file_name
 
-    tse_file = obtain_tse_file()
-    attribs = {}
+    def obtain_tse_file(self, task):
+        cft_file_name = self.obtain_cft_file_name(task)
+        file_name = cft_file_name + '.tse'
+        tse_file = path.join(task.ActiveDirectory, file_name)
 
-    with open(tse_file) as f:
-        for line in f:
-            if line.strip() == start:
-                break
+        return tse_file
 
-        for line in f:
-            if line.strip() == end:
-                break
+    def extract_attribs(self, task, start, end):
 
-            data = line.strip().split(' = ')
-            attribs[data[0]] = data[1]
+        tse_file = self.obtain_tse_file(task)
 
-    return attribs
+        attribs = {}
+        with open(tse_file) as f:
+            for line in f:
+                if line.strip() == start:
+                    break
 
+            for line in f:
+                if line.strip() == end:
+                    break
 
-def extract_machine_data_attribs():
-    machine_data_attribs = extract_attribs('MACHINE DATA:', 'END')
+                data = line.strip().split(' = ')
+                new_keys = re.sub(' ', '_', data[0])
+                attribs[new_keys] = data[1]
 
-    return machine_data_attribs
-
-
-def extract_hub_attribs():
-    hub_attribs = extract_attribs('HUB:', 'END')
-
-    return hub_attribs
-
-
-def extract_shroud_attribs():
-    shroud_attribs = extract_attribs('SHROUD:', 'END')
-
-    return shroud_attribs
+        return attribs
 
 
-def extract_main_blade_attribs():
-    main_blade_attribs = extract_attribs('BLADE:Main blade', 'END')
+class Attribs(Geometry):
+    def __init__(self, task):
+        Geometry.__init__(self, task)
+        self.machine_data_attribs = Geometry(task).extract_attribs(task, 'MACHINE DATA:', 'END')
+        self.hub_attribs = Geometry(task).extract_attribs(task, 'HUB:', 'END')
+        self.shroud_attribs = Geometry(task).extract_attribs(task, 'SHROUD:', 'END')
+        self.main_blade_attribs = Geometry(task).extract_attribs(task, 'BLADE:Main blade', 'END')
+        self.splitter_blade_attribs = Geometry(task).extract_attribs(task, 'BLADE:Splitter blade', 'END')
 
-    return main_blade_attribs
-
-
-def extract_splitter_blade_attribs():
-    splitter_blade_attribs = extract_attribs('BLADE:Splitter blade', 'END')
-
-    return splitter_blade_attribs
-
-
-def make_s4():
-
-    splitter_blade_attribs = extract_splitter_blade_attribs()
-    main_blade_attribs = extract_main_blade_attribs()
-
-    if splitter_blade_attribs == {}:
-        blades_per_set = int(main_blade_attribs['Blade Number'])
-        s4 = 'Number of Blades Per Set: {}'.format(blades_per_set + 1)
-    else:
-        blades_per_set = int(splitter_blade_attribs['Blade Number'])
-        s4 = 'Number of Blades Per Set: {}'.format(blades_per_set + 1)
-
-    return s4
+        self.MachineData = type('MachineData', (), self.machine_data_attribs)
+        self.Hub = type('Hub', (), self.hub_attribs)
+        self.Shroud = type('Shroud', (), self.shroud_attribs)
+        self.MainBlade = type('MainBlade', (), self.main_blade_attribs)
 
 
-def make_s8():
+class FilesStrings(Geometry):
+    def __init__(self, task):
+        Geometry.__init__(self, task)
+        self.s1 = '!======  CFTurbo Export  ========'
+        self.s2 = 'Axis of Rotation: {}'.format(Attribs(task).MachineData.Principal_Axis)
+        self.s3 = 'Number of Blade Sets: {}'.format(Attribs(task).MachineData.Bladeset_Count)
+        self.s5 = 'Blade Loft Direction: Spanwise'
+        geometry_units = Attribs(task).Hub.Input_Length_Units
+        self.s6 = 'Geometry Units: {}'.format(geometry_units.upper())
+        self.s7 = 'Coordinate System Orientation: Righthanded'
+        self.s12 = 'Hub Data File: {}'.format(path.basename(Attribs(task).Hub.Input_Filename))
+        self.s13 = 'Shroud Data File: {}'.format(path.basename(Attribs(task).Shroud.Input_Filename))
+        self.s14 = 'Profile Data File: {}'.format(path.basename(Attribs(task).MainBlade.Input_Filename))
 
-    main_blade_attribs = extract_main_blade_attribs()
+    def make_s4(self, task):
 
-    blade_0_le_type = main_blade_attribs['Leading Edge Type']
-    if blade_0_le_type == 'Single':
-        blade_0_le_type = 'EllipseEnd'
-    else:
-        blade_0_le_type = 'CutOffEnd'
-    s8 = 'Blade 0 LE: {}'.format(blade_0_le_type)
-
-    return s8
-
-
-def make_s9():
-
-    main_blade_attribs = extract_main_blade_attribs()
-
-    blade_0_te_type = main_blade_attribs['Trailing Edge Type']
-    if blade_0_te_type == 'Single':
-        blade_0_te_type = 'EllipseEnd'
-    else:
-        blade_0_te_type = 'CutOffEnd'
-    s9 = 'Blade 0 LE: {}'.format(blade_0_te_type)
-
-    return s9
-
-
-def make_s10():
-
-    splitter_blade_attribs = extract_splitter_blade_attribs()
-
-    if splitter_blade_attribs == {}:
-        return
-    else:
-        blade_1_le_type = splitter_blade_attribs['Leading Edge Type']
-        if blade_1_le_type == 'Single':
-            blade_1_le_type = 'EllipseEnd'
+        if Attribs(task).splitter_blade_attribs == {}:
+            blades_per_set = int(Attribs(task).MainBlade.Blade_Number)
+            s4 = 'Number of Blades Per Set: {}'.format(blades_per_set + 1)
         else:
-            blade_1_le_type = 'CutOffEnd'
-        s10 = 'Blade 1 LE: {}'.format(blade_1_le_type)
+            SplitterBlade = type('SplitterBlade', (), Attribs(task).splitter_blade_attribs)
+            blades_per_set = int(SplitterBlade.Blade_Number)
+            s4 = 'Number of Blades Per Set: {}'.format(blades_per_set + 1)
 
-    return s10
+        return s4
 
+    def make_s8_s9(self, edgeType, LeTe):
 
-def make_s11():
-
-    splitter_blade_attribs = extract_splitter_blade_attribs()
-
-    if splitter_blade_attribs == {}:
-        return
-    else:
-        blade_1_te_type = splitter_blade_attribs['Trailing Edge Type']
-        if blade_1_te_type == 'Single':
-            blade_1_te_type = 'EllipseEnd'
+        blade_type = edgeType
+        if blade_type == 'Single':
+            blade_type = 'EllipseEnd'
         else:
-            blade_1_te_type = 'CutOffEnd'
-        s11 = 'Blade 1 TE: {}'.format(blade_1_te_type)
+            blade_type = 'CutOffEnd'
+        s8_s9 = 'Blade 0 {}: {}'.format(LeTe, blade_type)
 
-    return s11
+        return s8_s9
 
-
-def create_inf_file(inf_file):
-
-    machine_data_attribs = extract_machine_data_attribs()
-    hub_attribs = extract_hub_attribs()
-    shroud_attribs = extract_shroud_attribs()
-    main_blade_attribs = extract_main_blade_attribs()
-
-    s1 = '!======  CFTurbo Export  ========'
-    s2 = 'Axis of Rotation: {}'.format(machine_data_attribs['Principal Axis'])
-    s3 = 'Number of Blade Sets: {}'.format(machine_data_attribs['Bladeset Count'])
-    s4 = make_s4()
-    s5 = 'Blade Loft Direction: Spanwise'
-    geometry_units = hub_attribs['Input Length Units']
-    s6 = 'Geometry Units: {}'.format(geometry_units.upper())
-    s7 = 'Coordinate System Orientation: Righthanded'
-    s8 = make_s8()
-    s9 = make_s9()
-    s10 = make_s10()
-    s11 = make_s11()
-    s12 = 'Hub Data File: {}'.format(path.basename(hub_attribs['Input Filename']))
-    s13 = 'Shroud Data File: {}'.format(path.basename(shroud_attribs['Input Filename']))
-    s14 = 'Profile Data File: {}'.format(path.basename(main_blade_attribs['Input Filename']))
-
-    with open(inf_file, 'w') as f:
-        f.write(s1 + '\n')
-        f.write(s2 + '\n')
-        f.write(s4 + '\n')
-        f.write(s5 + '\n')
-        f.write(s6 + '\n')
-        f.write(s7 + '\n')
-        f.write(s8 + '\n')
-        f.write(s9 + '\n')
-
-        if s10 is None:
-            pass
+    def make_s10(self, task):
+        if Attribs(task).splitter_blade_attribs == {}:
+            return
         else:
-            f.write(s10 + '\n')
-            f.write(s11 + '\n')
+            SplitterBlade = type('SplitterBlade', (), Attribs(task).splitter_blade_attribs)
+            blade_1_le_type = SplitterBlade.Leading_Edge_Type
+            if blade_1_le_type == 'Single':
+                blade_1_le_type = 'EllipseEnd'
+            else:
+                blade_1_le_type = 'CutOffEnd'
+            s10 = 'Blade 1 LE: {}'.format(blade_1_le_type)
 
-        f.write(s12 + '\n')
-        f.write(s13 + '\n')
-        f.write(s14 + '\n')
+        return s10
+
+    def make_s11(self, task):
+        if Attribs(task).splitter_blade_attribs == {}:
+            return
+        else:
+            SplitterBlade = type('SplitterBlade', (), Attribs(task).splitter_blade_attribs)
+            blade_1_te_type = SplitterBlade.Trailing_Edge_Type
+            if blade_1_te_type == 'Single':
+                blade_1_te_type = 'EllipseEnd'
+            else:
+                blade_1_te_type = 'CutOffEnd'
+            s11 = 'Blade 1 TE: {}'.format(blade_1_te_type)
+
+        return s11
+
+    def create_inf_file(self, task):
+        cft_file_name = Geometry(task).obtain_cft_file_name(task)
+        inf_file = cft_file_name + '.inf'
+        active_dir = path.join(task.ActiveDirectory, inf_file)
+
+        mainBlade = Attribs(task).MainBlade
+
+        with open(active_dir, 'w') as f:
+            f.write(self.s1 + '\n')
+            f.write(self.s2 + '\n')
+            f.write(self.s3 + '\n')
+            f.write(self.make_s4(task) + '\n')
+            f.write(self.s5 + '\n')
+            f.write(self.s6 + '\n')
+            f.write(self.s7 + '\n')
+            f.write(self.make_s8_s9(mainBlade.Leading_Edge_Type, 'LE') + '\n')
+            f.write(self.make_s8_s9(mainBlade.Trailing_Edge_Type, 'TE') + '\n')
+
+            if self.make_s10(task) is None:
+                pass
+            else:
+                f.write(self.make_s10(task) + '\n')
+                f.write(self.make_s11(task) + '\n')
+
+            f.write(self.s12 + '\n')
+            f.write(self.s13 + '\n')
+            f.write(self.s14 + '\n')
 
 
-create_inf_file('tets.inf')
+
+
+
---
+++ CFTurboExtension_kp/visibility.py
@@ -0,0 +1,138 @@
+# I've commented it because it is possible to parameterized tip clearance in TurboGrid
+# def tipClearanceVisible(task, property):
+#     tipClearance = MainDimensions(task).tipClearanceExist(task)
+#     if tipClearance == '0':
+#         return False
+#     return True
+
+def HubDiameterVisible(task, property):
+    HubDiameter = MainDimensions(task).mainDimExist(task, 'dN', 'Desc', 'Hub diameter dH')
+    if HubDiameter is None:
+        return False
+    return True
+
+def SuctionDiameterVisible(task, property):
+    SuctionDiameter = MainDimensions(task).mainDimExist(task, 'dS', 'Desc', 'Suction diameter dS')
+    if SuctionDiameter is None:
+        return False
+    return True
+
+def ImpellerDiameterVisible(task, property):
+    ImpellerDiameter = MainDimensions(task).mainDimExist(task, 'd2', 'Desc', 'Impeller diameter d2')
+    if ImpellerDiameter is None:
+        return False
+    return True
+
+def ImpellerOutWidthVisible(task, property):
+    ImpellerOutWidth = MainDimensions(task).mainDimExist(task, 'b2', 'Desc', 'Impeller outlet width b2')
+    if ImpellerOutWidth is None:
+        return False
+    return True
+
+def HubDiameterInletVisible(task, property):
+    HubDiameterInlet = MainDimensions(task).mainDimExist(task, 'dN', 'Desc', 'Hub diameter inlet dH1')
+    if HubDiameterInlet is None:
+        return False
+    return True
+
+def TipDiameterInletVisible(task, property):
+    TipDiameterInlet = MainDimensions(task).mainDimExist(task, 'dS', 'Desc', 'Tip diameter inlet dS1')
+    if TipDiameterInlet is None:
+        return False
+    return True
+
+def HubDiameterOutVisible(task, property):
+    HubDiameterOut = MainDimensions(task).mainDimExist(task, 'dH2', 'Desc', 'Hub diameter outlet dH2')
+    if HubDiameterOut is None:
+        return False
+    return True
+
+def TipDiameterOutVisible(task, property):
+    TipDiameterOut = MainDimensions(task).mainDimExist(task, 'dS2', 'Desc', 'Tip diameter outlet dS2')
+    if TipDiameterOut is None:
+        return False
+    return True
+
+def beta1Visible(task, property):
+    beta1 = BladeProperties(task).betaExist(task, 'Beta1')
+    if beta1 is None:
+        return False
+    return True
+
+def beta2Visible(task, property):
+    beta1 = BladeProperties(task).betaExist(task, 'Beta2')
+    if beta1 is None:
+        return False
+    return True
+
+def LePosHubVisible(task, property):
+    LePosHub = Meridian(task).positionExist(task, 'LePosHub')
+    if LePosHub is None:
+        return False
+    return True
+
+def TePosHubVisible(task, property):
+    TePosHub = Meridian(task).positionExist(task, 'TePosHub')
+    if TePosHub is None:
+        return False
+    return True
+
+def LePosHubSplitterVisible(task, property):
+    LePosHubSplitter = Meridian(task).positionExist(task, 'LePosHubSplitter')
+    if LePosHubSplitter is None:
+        return False
+    return True
+
+def BladeThickHub1Visible(task, property):
+    BladeThickHub1 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickHub_1')
+    if BladeThickHub1 is None:
+        return False
+    return True
+
+def BladeThickHub2Visible(task, property):
+    BladeThickHub2 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickHub_2')
+    if BladeThickHub2 is None:
+        return False
+    return True
+
+def BladeThickHub3Visible(task, property):
+    BladeThickHub3 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickHub_3')
+    if BladeThickHub3 is None:
+        return False
+    return True
+
+def BladeThickHub4Visible(task, property):
+    BladeThickHub4 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickHub_4')
+    if BladeThickHub4 is None:
+        return False
+    return True
+
+def BladeThickShroud1Visible(task, property):
+    BladeThickShroud1 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickShroud_1')
+    if BladeThickShroud1 is None:
+        return False
+    return True
+
+def BladeThickShroud2Visible(task, property):
+    BladeThickShroud2 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickShroud_2')
+    if BladeThickShroud2 is None:
+        return False
+    return True
+
+def BladeThickShroud3Visible(task, property):
+    BladeThickShroud3 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickShroud_3')
+    if BladeThickShroud3 is None:
+        return False
+    return True
+
+def BladeThickShroud4Visible(task, property):
+    BladeThickShroud4 = BladeProfiles(task).bladeThicknessExist(task, 'BladeThickShroud_4')
+    if BladeThickShroud4 is None:
+        return False
+    return True
+
+def BladeProfilesVisible(task, property):
+    num_points = BladeProfiles(task).BladeProfilesExist(task)
+    if num_points == 1:
+        return True
+    return False
