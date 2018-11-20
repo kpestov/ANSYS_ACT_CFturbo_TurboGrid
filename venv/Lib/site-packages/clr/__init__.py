928768b0376643ba1517a4fae0b197892a8ba455
kpestov <kpestov91@gmail.com>
2018-11-20 21:38:59 +0500

    removed venv folder

--- venv/Lib/site-packages/clr/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from clr.style_builder import StyleBuilder, _styles
-
-
-__module__ = sys.modules[__name__]
-for key, value in _styles.items():
-    setattr(__module__, key, StyleBuilder([value], True))
